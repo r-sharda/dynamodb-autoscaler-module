e78d5bec8a9f146d6046a9a3d1c79fcde9154310
Your Name <you@example.com>
2018-07-13 21:16:14 +0530

    dynamodb-autosacler-module

---
+++ variables.tf
@@ -0,0 +1,83 @@
+variable "namespace" {
+  type        = "string"
+  description = "Namespace (e.g. `cp` or `cloudposse`)"
+}
+
+variable "stage" {
+  type        = "string"
+  description = "Stage (e.g. `prod`, `dev`, `staging`, `infra`)"
+}
+
+variable "name" {
+  type        = "string"
+  description = "Name  (e.g. `app` or `cluster`)"
+}
+
+variable "delimiter" {
+  type        = "string"
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `stage`, `name` and `attributes`"
+}
+
+variable "attributes" {
+  type        = "list"
+  default     = []
+  description = "Additional attributes (e.g. `1`)"
+}
+
+variable "tags" {
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
+}
+
+variable "dynamodb_table_name" {
+  type        = "string"
+  description = "DynamoDB table name"
+}
+
+variable "dynamodb_table_arn" {
+  type        = "string"
+  description = "DynamoDB table ARN"
+}
+
+variable "dynamodb_indexes" {
+  type        = "list"
+  description = "List of DynamoDB indexes"
+  default     = []
+}
+
+variable "autoscale_write_target" {
+  default     = 50
+  description = "The target value for DynamoDB write autoscaling"
+}
+
+variable "autoscale_read_target" {
+  default     = 50
+  description = "The target value for DynamoDB read autoscaling"
+}
+
+variable "autoscale_min_read_capacity" {
+  default     = 5
+  description = "DynamoDB autoscaling min read capacity"
+}
+
+variable "autoscale_max_read_capacity" {
+  default     = 20
+  description = "DynamoDB autoscaling max read capacity"
+}
+
+variable "autoscale_min_write_capacity" {
+  default     = 5
+  description = "DynamoDB autoscaling min write capacity"
+}
+
+variable "autoscale_max_write_capacity" {
+  default     = 20
+  description = "DynamoDB autoscaling max write capacity"
+}
+
+variable "enabled" {
+  description = "Set to false to prevent the module from creating any resources"
+  default     = "true"
+}
